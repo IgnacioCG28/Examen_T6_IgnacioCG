0cbeecd4dba1da77da8f6a3b08da1d7adfa784f7
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2024-02-21 09:59:43 +0100

    js show images

--- build/Scripts/main.js
+++ build/Scripts/main.js
@@ -6,4 +6,24 @@ function startApp() {
     createGallery();
 }
 
+function createGallery() {
+    const gallery = document.querySelector('.image-gallery');
+
+    for(let i = 1; i <= 6; i++ ) {
+        const image = document.createElement('picture');
+        image.innerHTML = `
+            <source srcset="build/Assets/Images/${i}.jpg" type="image/jpg">
+            <source srcset="Assets/Images/${i}.webp" type="image/webp">
+            <source srcset="Assets/Images/${i}.avif" type="image/avif">
+            <img loading="lazy" width="200" height="300" src="Assets/Images/${i}.jpg" alt="Film image">
+        `;
+        image.onclick = function() {
+            showImage(i);
+        }
+
+        gallery.appendChild(image);
+    }
+}  
+
+     
 
