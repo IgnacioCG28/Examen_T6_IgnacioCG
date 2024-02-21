a8ad980f2a22b108e3028800d8fbd3387b329385
IgnacioCG28 <ignaciocarmonagonzalez@alumnos.tuniversformacion.es>
2024-02-21 10:19:55 +0100

    overlay

--- build/Scripts/main.js
+++ build/Scripts/main.js
@@ -13,9 +13,9 @@ function createGallery() {
         const image = document.createElement('picture');
         image.innerHTML = `
             <source srcset="build/Assets/Images/${i}.jpg" type="image/jpg">
-            <source srcset="Assets/Images/${i}.webp" type="image/webp">
-            <source srcset="Assets/Images/${i}.avif" type="image/avif">
-            <img loading="lazy" width="200" height="300" src="Assets/Images/${i}.jpg" alt="Film image">
+            <source srcset="build/Assets/Images/${i}.webp" type="image/webp">
+            <source srcset="build/Assets/Images/${i}.avif" type="image/avif">
+            <img loading="lazy" width="200" height="300" src="build/Assets/Images/${i}.jpg" alt="Film image">
         `;
         image.onclick = function() {
             showImage(i);
@@ -28,12 +28,20 @@ function createGallery() {
 function showImage(id) {
     const image = document.createElement('picture');
     image.innerHTML = `
-    <source srcset="Assets/Images/BigImages/${id}.jpg" type="image/jpg">
-    <source srcset="Assets/Images/BigImages/${id}.webp" type="image/webp">
-    <source srcset="Assets/Images/BigImages/${id}.avif" type="image/avif">
-    <img loading="lazy" width="200" height="300" src="Assets/Images/BigImages/${id}.jpg" alt="Film image">
+    <source srcset="build/Assets/Images/BigImages/${id}.jpg" type="image/jpg">
+    <source srcset="build/Assets/Images/BigImages/${id}.webp" type="image/webp">
+    <source srcset="build/Assets/Images/BigImages/${id}.avif" type="image/avif">
+    <img loading="lazy" width="200" height="300" src="build/Assets/Images/BigImages/${id}.jpg" alt="Film image">
 `;
 
 const overlay = document.createElement('div')
-   
+overlay.appendChild(image);
+overlay.classList.add('overlay');
+overlay.onclick = function() {
+    const body = document.querySelector('body');
+    body.classList.remove('fixed-body');
+    overlay.remove();
+}
+
+}
 
